569b7633259429d17d4ae005148f0fd58c7f5c82
Peter Keating <peter@moov2.com>
2014-12-11 19:26:22 +0000

    created a simple Nancy project that returns a static HTML page.

---
+++ src/Erik/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Nancy;
+using Nancy.Authentication.Forms;
+using Nancy.Bootstrapper;
+using Nancy.Cryptography;
+using Nancy.Session;
+using Nancy.TinyIoc;
+
+namespace Erik
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        /// <summary>
+        /// Runs when the application first spins up.
+        /// </summary>
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+
+        }
+
+        /// <summary>
+        /// Runs when a request is made to the server.
+        /// </summary>
+        protected override void RequestStartup(TinyIoCContainer requestContainer, IPipelines pipelines, NancyContext context)
+        {
+
+        }
+    }
+}
---
+++ src/Erik/Modules/DefaultModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using Nancy.Authentication.Forms;
+using Nancy.ModelBinding;
+using Nancy.Security;
+using System;
+using System.IO;
+
+namespace Erik.Modules
+{
+    public class DefaultModule : NancyModule
+    {
+        public DefaultModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return Response.AsFile("index.html");
+            };
+        }
+    }
+}
