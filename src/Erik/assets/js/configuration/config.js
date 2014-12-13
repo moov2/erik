41c795f9ffeb0e78e7666443677a6a01b241f8fb
DannyT <dan@moov2.com>
2014-12-13 01:30:54 +0000

    removed massive test image that was used as old background

--- src/Erik/assets/js/configuration/config.js
+++ src/Erik/assets/js/configuration/config.js
@@ -2,8 +2,8 @@ module.exports = {
     /**
      * Dimensions of the game.
      */
-    CANVAS_HEIGHT: 600,
-    CANVAS_WIDTH: 800,
+    CANVAS_HEIGHT: '100%',
+    CANVAS_WIDTH: '100%',
 
     /**
      * Id of element in the DOM that the canvas should be placed into.
